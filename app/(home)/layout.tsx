5653c5fd00fd9aaaebcdc04eb3ac9c1f06d4acd5
Diego-Paris <diego.paris@upr.edu>
2024-02-24 23:12:53 -0400

    added how to lock out unauthorized users

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -5,6 +5,12 @@ import { HeaderMegaMenu } from '@/components/HeaderMegaMenu/HeaderMegaMenu';
 import { authOptions } from '../api/auth/[...nextauth]/route';
 
 export default async function HomeLayout({ children }: { children: any }) {
+  // const session = await getServerSession(authOptions);
+
+  // if (!session) {
+  //   redirect('/login');
+  // }
+
   return (
     <>
       <HeaderMegaMenu />
