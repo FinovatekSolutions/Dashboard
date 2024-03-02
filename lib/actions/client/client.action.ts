beeeec6ade4a0f31c697683234de7d41aaf9ceef
Diego-Paris <diego.paris@upr.edu>
2024-03-02 16:32:27 -0400

    improved client actions

--- lib/actions/client/client.action.ts
+++ lib/actions/client/client.action.ts
@@ -18,24 +18,24 @@ export async function getClientById(clientId: string): Promise<Client | null> {
   });
 }
 
-export async function createClient(input: Prisma.ClientCreateInput): Promise<void> {
+export async function createClient(input: Prisma.ClientCreateInput): Promise<Client> {
   const data = ClientCreateInputSchema.parse(input);
 
-  await prisma.client.create({ data });
+  return prisma.client.create({ data });
 }
 
-export async function updateClient(input: Prisma.ClientUpdateInput): Promise<void> {
+export async function updateClient(input: Prisma.ClientUpdateInput): Promise<Client> {
   const data = ClientUpdateInputSchema.parse(input);
-  const id = z.string().parse(data.id);
+  const clientId = z.string().parse(data.id); // Make sure that data.id can only be string
 
-  await prisma.client.update({
-    where: { id },
+  // throws an error if no client is found
+  return prisma.client.update({
+    where: { id: clientId },
     data,
   });
 }
 
-export async function removeClient(clientId: string): Promise<void> {
-  const id = z.string().parse(clientId);
-
-  await prisma.client.delete({ where: { id } });
+export async function removeClient(clientId: string): Promise<Client> {
+  // throws an error if no client is found
+  return prisma.client.delete({ where: { id: clientId } });
 }
