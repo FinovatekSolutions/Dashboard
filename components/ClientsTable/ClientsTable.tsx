919582c1b0e14d4f049d215205cc73ccb0668b91
Diego-Paris <diego.paris@upr.edu>
2024-03-05 14:31:37 -0400

    added actions to client table to view single client

--- components/ClientsTable/ClientsTable.tsx
+++ components/ClientsTable/ClientsTable.tsx
@@ -1,10 +1,33 @@
-import { useMemo } from 'react';
+'use client';
+
+import { useEffect, useMemo } from 'react';
 import { MantineReactTable, useMantineReactTable, type MRT_ColumnDef } from 'mantine-react-table';
+import { notifications } from '@mantine/notifications';
+
 import { Client } from '@prisma/client';
+
+import { ActionIcon, Flex, Tooltip, useMantineTheme } from '@mantine/core';
+import { IconEdit, IconEye, IconTrash, IconX } from '@tabler/icons-react';
+
 import { useGetClients } from '@/lib/actions/client';
+import { useRouter } from 'next/navigation';
 
 const ClientsTable = () => {
   const getClientsQuery = useGetClients();
+  const theme = useMantineTheme();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (getClientsQuery.isError) {
+      notifications.show({
+        color: 'red',
+        title: 'Failed to fetch data',
+        message: 'An error occurred. Please try again later.',
+        icon: <IconX size={theme.fontSizes.md} />,
+        autoClose: 4000,
+      });
+    }
+  }, [getClientsQuery.isError]);
 
   //should be memoized or stable
   const columns = useMemo<MRT_ColumnDef<Client>[]>(
@@ -56,6 +79,9 @@ const ClientsTable = () => {
   const table = useMantineReactTable({
     columns,
     data: getClientsQuery.data || [], //must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
+    positionGlobalFilter: 'left',
+    enableRowActions: true,
+    getRowId: (originalRow) => originalRow.id,
     initialState: {
       showGlobalFilter: true,
     },
@@ -64,7 +90,6 @@ const ClientsTable = () => {
       isSaving: getClientsQuery.isLoading,
       showProgressBars: getClientsQuery.isFetching,
     },
-    positionGlobalFilter: 'left',
     mantineSearchTextInputProps: {
       placeholder: `Search ${getClientsQuery?.data?.length || 0} rows`,
       id: 'wazzapp',
@@ -77,6 +102,19 @@ const ClientsTable = () => {
       shadow: 'none',
       style: { border: '0px solid #e0e0e0' },
     },
+    renderRowActions: ({ row }) => (
+      <Flex gap="md">
+        <Tooltip label="View Client">
+          <ActionIcon
+            onClick={() => router.push(`/clients/${row.id}`)}
+            color={theme.colors['trust-md-light-blue'][4]}
+            variant="outline"
+          >
+            <IconEye />
+          </ActionIcon>
+        </Tooltip>
+      </Flex>
+    ),
   });
 
   return (
