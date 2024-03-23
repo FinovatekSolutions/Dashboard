b9a34d3da7dc3431113cebf84eb230942bb88dd3
Diego-Paris <diego.paris@upr.edu>
2024-03-22 21:33:28 -0400

    Added styling and responsiveness to admin navbar

--- components/AdminNavBar/AdminNavBar.tsx
+++ components/AdminNavBar/AdminNavBar.tsx
@@ -1,44 +1,119 @@
 'use client';
 
-import { AppShell, Burger, Group, Skeleton, useMantineTheme } from '@mantine/core';
+import {
+  Anchor,
+  Button,
+  Text,
+  AppShell,
+  Burger,
+  Group,
+  Skeleton,
+  useMantineTheme,
+  Divider,
+  Stack,
+  Space,
+  rem,
+  Image,
+} from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { MantineLogo } from '@mantinex/mantine-logo';
+// import Image from 'next/image';
+import Link from 'next/link';
+import classes from './AdminNavBar.module.css';
+import { UserMenu } from '../UserMenu/UserMenu';
 
 export function AdminNavBar({ children }: { children: any }) {
   const theme = useMantineTheme();
-  const [opened, { toggle }] = useDisclosure(false);
+  const [mobileOpened, { toggle: toggleMobile, close: closeMobile }] = useDisclosure();
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
 
   return (
     <>
       <AppShell
-        header={{ height: 60 }}
-        navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
-        padding="md"
+        header={{ height: { base: rem(60), sm: rem(90) } }}
+        navbar={{
+          width: 300,
+          breakpoint: 'sm',
+          collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+        }}
         withBorder={false}
         styles={{
           header: {
             backgroundColor: '#1A1C27',
+            marginBottom: '12px',
+            boxShadow: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
           },
           navbar: {
-            backgroundColor: '#192431',
+            backgroundColor: theme.colors.dark[7],
           },
         }}
       >
         <AppShell.Header>
-          <Group h="100%" px="md">
-            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-            <MantineLogo size={30} />
+          <Group justify="space-between" h="100%" px="md">
+            <Group h="100%" gap={10}>
+              <Burger
+                color={theme.colors.gray[2]}
+                opened={mobileOpened}
+                onClick={toggleMobile}
+                hiddenFrom="sm"
+                size="sm"
+              />
+              <Burger
+                color={theme.colors.gray[2]}
+                opened={desktopOpened}
+                onClick={toggleDesktop}
+                visibleFrom="sm"
+                size="sm"
+              />
+              <Space />
+              <Image
+                src="/trustmd-tree.png"
+                alt="TrustMD Logo"
+                w={{ base: 30, sm: 40 }}
+                h={{ base: 30, sm: 40 }}
+              />
+              <Text c={theme.colors.gray[2]}>Admin Dashboard</Text>
+            </Group>
+            <Group>
+              <UserMenu />
+            </Group>
           </Group>
         </AppShell.Header>
         <AppShell.Navbar p="md">
-          Navbar
-          {Array(15)
-            .fill(0)
-            .map((_, index) => (
-              <Skeleton key={index} h={28} mt="sm" animate={false} />
-            ))}
+          <Text c={theme.colors.gray[2]}>Navigation</Text>
+
+          <Divider my="md" color={theme.colors.dark[3]} />
+          <Button component={Link} href="/" color="teal" size="md">
+            Return to Home
+          </Button>
+          <Divider my="md" color={theme.colors.dark[3]} />
+          <Stack>
+            <Button
+              component={Link}
+              href="/admin"
+              color="teal"
+              variant="subtle"
+              size="md"
+              onClick={closeMobile}
+            >
+              General
+            </Button>
+            <Button
+              component={Link}
+              href="/admin/permissions"
+              color="teal"
+              variant="subtle"
+              size="md"
+              onClick={closeMobile}
+            >
+              Permissions
+            </Button>
+          </Stack>
         </AppShell.Navbar>
-        <AppShell.Main>{children}</AppShell.Main>
+        <AppShell.Main>
+          <Space h="sm" />
+          {children}
+        </AppShell.Main>
       </AppShell>
     </>
   );
