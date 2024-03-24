bd923ef37c7a56bba7bb3f56271903b98406d383
Diego-Paris <diego.paris@upr.edu>
2024-03-24 02:29:13 -0400

    admin layout can now only be accessed by admins

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,23 +1,29 @@
-import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
-
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
-import { MantineLogo } from '@mantinex/mantine-logo';
+import { Role } from '@prisma/client';
 import { authOptions } from '../api/auth/[...nextauth]/route';
 
 import { LockWhenSignedOut } from '@/lib/providers/lock-when-signed-out';
 import { AdminNavBar } from '@/components/global/AdminNavBar/AdminNavBar';
+import { LockWhenNotAdmin } from '@/lib/providers/lock-when-not-admin';
+import { getPermissionByEmail } from '@/lib/actions/permission';
 
 export default async function AdminLayout({ children }: { children: any }) {
   const session = await getServerSession(authOptions);
+  const permission = await getPermissionByEmail(session?.user?.email || '');
 
   if (!session) {
     redirect('/login');
   }
 
+  if (!permission || permission.role !== Role.ADMIN) {
+    redirect('/');
+  }
+
   return (
     <>
       <LockWhenSignedOut />
+      <LockWhenNotAdmin />
       <AdminNavBar>{children}</AdminNavBar>
     </>
   );
