4a62c8cacf290ecc91b937c5806547041a983146
Diego-Paris <diego.paris@upr.edu>
2024-03-27 11:05:11 -0400

    updated visually general admin page

--- app/admin/page.client.tsx
+++ app/admin/page.client.tsx
@@ -1,18 +1,34 @@
 'use client';
 
 import type { ReactElement } from 'react';
-import { Title, Text } from '@mantine/core';
+import { Title, Text, Paper, Center, useMantineTheme } from '@mantine/core';
 
 import { Welcome } from '@/components/examples/Welcome/Welcome';
 
 export function AdminClient(): ReactElement {
+  const theme = useMantineTheme();
+
   return (
     <>
       <Title ta="center" mt={100}>
-        Welcome to{' '}
-        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Admin Dashboard
-        </Text>
+        <Center>
+          <Paper
+            w="fit-content"
+            p="1rem"
+            style={{
+              backgroundColor: theme.colors.dark[7],
+            }}
+          >
+            <Text
+              inherit
+              variant="gradient"
+              component="span"
+              gradient={{ from: theme.colors['trust-md-light-blue'][4], to: theme.colors['trust-md-light-green'][7] }}
+            >
+              Admin Dashboard
+            </Text>
+          </Paper>
+        </Center>
       </Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
         This dashboard allows you to edit and manage your website content.
