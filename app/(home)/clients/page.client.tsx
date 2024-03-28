1eb41328e9fe7a03c3dee383d1122308b1e18e07
Diego-Paris <diego.paris@upr.edu>
2024-03-28 12:54:38 -0400

    Made a global page container for each page to have consistent styling

--- app/(home)/clients/page.client.tsx
+++ app/(home)/clients/page.client.tsx
@@ -17,46 +17,44 @@ import { IconRefresh } from '@tabler/icons-react';
 import ClientsTable from '@/components/client/general/ClientsTable/ClientsTable';
 import CreateClientButton from '@/components/client/crud/CreateClientButton/CreateClientButton';
 import { useGetClients } from '@/lib/actions/client';
+import { PageContainer } from '@/components/global/PageContainer/PageContainer';
 
 export function ClientsClient(): ReactElement {
   const theme = useMantineTheme();
   const getClientsQuery = useGetClients();
 
   return (
-    <Center>
-      <Space h="md" />
-      <Paper p="xs" shadow="xl" withBorder w={{ base: '97%' }} maw={`${theme.breakpoints.lg}`}>
-        <Title m={10} order={1}>
-          Clients
-        </Title>
-        {/* Container for the first two components */}
-        <Flex
-          direction={{ base: 'column', sm: 'row' }}
-          justify="space-between"
-          style={{ gap: '16px' }} // Adjust the gap size as needed
-          mt={16}
+    <PageContainer>
+      <Title m={10} order={1}>
+        Clients
+      </Title>
+      {/* Container for the first two components */}
+      <Flex
+        direction={{ base: 'column', sm: 'row' }}
+        justify="space-between"
+        style={{ gap: '16px' }} // Adjust the gap size as needed
+        mt={16}
+      >
+        {/* Component 1 */}
+        <CreateClientButton />
+
+        {/* Component 2 */}
+        <Button
+          onClick={() => getClientsQuery.refetch()}
+          leftSection={<IconRefresh size={14} />}
+          variant="default"
+          size="md"
         >
-          {/* Component 1 */}
-          <CreateClientButton />
+          Refresh
+        </Button>
+      </Flex>
 
-          {/* Component 2 */}
-          <Button
-            onClick={() => getClientsQuery.refetch()}
-            leftSection={<IconRefresh size={14} />}
-            variant="default"
-            size="md"
-          >
-            Refresh
-          </Button>
-        </Flex>
-
-        {/* Component 3 - Always below the first two */}
-        <Divider my="md" />
-        <Box style={{ marginTop: '16px' }}>
-          {/* Adjust the margin as needed */}
-          <ClientsTable />
-        </Box>
-      </Paper>
-    </Center>
+      {/* Component 3 - Always below the first two */}
+      <Divider my="md" />
+      <Box style={{ marginTop: '16px' }}>
+        {/* Adjust the margin as needed */}
+        <ClientsTable />
+      </Box>
+    </PageContainer>
   );
 }
--- app/admin/permissions/page.client.tsx
+++ app/admin/permissions/page.client.tsx
@@ -17,49 +17,47 @@ import { IconRefresh } from '@tabler/icons-react';
 import PermissionsTable from '@/components/permission/general/PermissionsTable/PermissionsTable';
 import CreatePermissionButton from '@/components/permission/crud/CreatePermissionButton/CreatePermissionButton';
 import { useGetPermissions } from '@/lib/actions/permission';
+import { PageContainer } from '@/components/global/PageContainer/PageContainer';
 
 export function ClientsClient(): ReactElement {
   const theme = useMantineTheme();
   const getPermissionsQuery = useGetPermissions();
 
   return (
-    <Center>
-      <Space h="md" />
-      <Paper p="xs" shadow="xl" withBorder w={{ base: '97%' }} maw={`${theme.breakpoints.lg}`}>
-        <Title m={10} order={1}>
-          Permissions
-        </Title>
-        <Text ml={10} c="dimmed">
-          Manage who has access to the website and their roles.
-        </Text>
-        {/* Container for the first two components */}
-        <Flex
-          direction={{ base: 'column', sm: 'row' }}
-          justify="space-between"
-          style={{ gap: '16px' }} // Adjust the gap size as needed
-          mt={16}
+    <PageContainer>
+      <Title m={10} order={1}>
+        Permissions
+      </Title>
+      <Text ml={10} c="dimmed">
+        Manage who has access to the website and their roles.
+      </Text>
+      {/* Container for the first two components */}
+      <Flex
+        direction={{ base: 'column', sm: 'row' }}
+        justify="space-between"
+        style={{ gap: '16px' }} // Adjust the gap size as needed
+        mt={16}
+      >
+        {/* Component 1 */}
+        <CreatePermissionButton />
+
+        {/* Component 2 */}
+        <Button
+          onClick={() => getPermissionsQuery.refetch()}
+          leftSection={<IconRefresh size={14} />}
+          variant="default"
+          size="md"
         >
-          {/* Component 1 */}
-          <CreatePermissionButton />
+          Refresh
+        </Button>
+      </Flex>
 
-          {/* Component 2 */}
-          <Button
-            onClick={() => getPermissionsQuery.refetch()}
-            leftSection={<IconRefresh size={14} />}
-            variant="default"
-            size="md"
-          >
-            Refresh
-          </Button>
-        </Flex>
-
-        {/* Component 3 - Always below the first two */}
-        <Divider my="md" />
-        <Box style={{ marginTop: '16px' }}>
-          {/* Adjust the margin as needed */}
-          <PermissionsTable />
-        </Box>
-      </Paper>
-    </Center>
+      {/* Component 3 - Always below the first two */}
+      <Divider my="md" />
+      <Box style={{ marginTop: '16px' }}>
+        {/* Adjust the margin as needed */}
+        <PermissionsTable />
+      </Box>
+    </PageContainer>
   );
 }
