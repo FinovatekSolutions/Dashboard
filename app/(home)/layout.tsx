4bfb748a3db539129cc5c362bb8e5d5e111eb2a6
Diego-Paris <diego.paris@upr.edu>
2024-04-07 22:05:59 -0400

    removed home lock

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { getServerSession } from 'next-auth';
-import { redirect } from 'next/navigation';
-import { authOptions } from '../api/auth/[...nextauth]/route';
-import { LockWhenSignedOut } from '@/lib/providers/lock-when-signed-out';
+// import { getServerSession } from 'next-auth';
+// import { redirect } from 'next/navigation';
+// import { authOptions } from '../api/auth/[...nextauth]/route';
+// import { LockWhenSignedOut } from '@/lib/providers/lock-when-signed-out';
 import { HomeNavBar } from '@/components/global/HomeNavBar/HomeNavBar';
 
 export default async function HomeLayout({ children }: { children: any }) {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  if (!session) {
-    redirect('/login');
-  }
+  // if (!session) {
+  //   redirect('/login');
+  // }
 
   return (
     <>
-      <LockWhenSignedOut />
+      {/* <LockWhenSignedOut /> */}
       <HomeNavBar>{children}</HomeNavBar>
     </>
   );
