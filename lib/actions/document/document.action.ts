bdaa2d224b0dc3f5ff8efb18fc404b58cc0c6e5c
Diego-Paris <diego.paris@upr.edu>
2024-04-12 23:54:11 -0400

    Added document actions

--- lib/actions/document/document.action.ts
+++ lib/actions/document/document.action.ts
@@ -1,54 +1,144 @@
 'use server';
 
-import type { Prisma, Client } from '@prisma/client';
-import { ClientCreateInputSchema, ClientUpdateInputSchema } from '@prisma/zod';
+import type { Prisma } from '@prisma/client';
+import { DocumentCreateInputSchema, DocumentUpdateInputSchema } from '@prisma/zod';
 import { z } from 'zod';
 
 import prisma from '@/lib/utils/prisma';
-import { ClientWithReviews } from '@/lib/utils/types';
+import { DocumentDetails, FullDocument } from '@/lib/utils/types';
 
-export async function getClients(): Promise<Client[]> {
-  return prisma.client.findMany({
-    orderBy: {
-      createdAt: 'desc', // Use 'desc' for descending order (newest first)
-    },
-  });
-}
-
-export async function getClientById(clientId: string): Promise<ClientWithReviews | null> {
-  return prisma.client.findUnique({
-    where: {
-      id: clientId,
-    },
-    include: {
-      reviews: {
-        // Includes the reviews for the client
-        include: {
-          user: true, // This includes the user for each review
+export async function getDocuments(): Promise<DocumentDetails[]> {
+  // Apparently Prisma does not have a way to exclude fields
+  // So all this could've been avoided if I was given
+  // the option to simply exclude the content field,
+  // instead I have to include EVERYTHING ELSE.
+  return prisma.document.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      published: true,
+      locked: true,
+      incidentDate: true,
+      language: true,
+      uploaderId: true,
+      uploader: {
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true,
+          email: true,
+          role: true,
         },
       },
+      editors: {
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true,
+          email: true,
+          role: true,
+        },
+      },
+      damages: {
+        select: {
+          id: true,
+          name: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      },
+      infrastructures: {
+        select: {
+          id: true,
+          name: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      },
+      towns: {
+        select: {
+          id: true,
+          name: true,
+          latitude: true,
+          longitude: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      },
+      events: {
+        select: {
+          id: true,
+          title: true,
+          date: true,
+          documentId: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      },
+      createdAt: true,
+      updatedAt: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
     },
   });
 }
 
-export async function createClient(input: Prisma.ClientCreateInput): Promise<Client> {
-  const data = ClientCreateInputSchema.parse(input);
-
-  return prisma.client.create({ data });
-}
-
-export async function updateClient(input: Prisma.ClientUpdateInput): Promise<Client> {
-  const data = ClientUpdateInputSchema.parse(input);
-  const clientId = z.string().parse(data.id); // Make sure that data.id can only be string
-
-  // throws an error if no client is found
-  return prisma.client.update({
-    where: { id: clientId },
-    data,
+export async function getFullDocumentById(documentId: string): Promise<FullDocument | null> {
+  return prisma.document.findUnique({
+    where: {
+      id: documentId,
+    },
+    include: {
+      towns: true,
+      events: true,
+      damages: true,
+      infrastructures: true,
+    },
   });
 }
 
-export async function removeClient(clientId: string): Promise<Client> {
-  // throws an error if no client is found
-  return prisma.client.delete({ where: { id: clientId } });
+export async function createDocument(input: Prisma.DocumentCreateInput): Promise<FullDocument> {
+  const data = DocumentCreateInputSchema.parse(input);
+
+  return prisma.document.create({
+    data,
+    include: {
+      towns: true,
+      events: true,
+      damages: true,
+      infrastructures: true,
+    },
+  });
+}
+
+export async function updateDocument(input: Prisma.DocumentUpdateInput): Promise<FullDocument> {
+  const data = DocumentUpdateInputSchema.parse(input);
+  const documentId = z.string().parse(data.id); // Make sure that data.id can only be string
+
+  // throws an error if not found
+  return prisma.document.update({
+    where: { id: documentId },
+    data,
+    include: {
+      towns: true,
+      events: true,
+      damages: true,
+      infrastructures: true,
+    },
+  });
+}
+
+export async function removeDocument(documentId: string): Promise<FullDocument> {
+  // throws an error if not found
+  return prisma.document.delete({
+    where: { id: documentId },
+    include: {
+      towns: true,
+      events: true,
+      damages: true,
+      infrastructures: true,
+    },
+  });
 }
