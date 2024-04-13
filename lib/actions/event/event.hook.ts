1e3443b728b10e6a93a5188e59d998055c768a5c
Diego-Paris <diego.paris@upr.edu>
2024-04-13 16:11:37 -0400

    added event actions and hooks

--- lib/actions/event/event.hook.ts
+++ lib/actions/event/event.hook.ts
@@ -1,48 +1,24 @@
-import type { UseQueryOptions, UseQueryResult } from '@tanstack/react-query';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import type { Prisma } from '@prisma/client';
 
-import {
-  getDocuments,
-  getFullDocumentById,
-  createDocument,
-  updateDocument,
-  removeDocument,
-} from '@/lib/actions/document';
-import {
-  onSuccessCallback,
-  onErrorCallback,
-  DocumentDetails,
-  FullDocument,
-} from '@/lib/utils/types';
-
-const getDocumentsQueryKey = 'getDocuments';
-const getDocumentByIdQueryKey = 'getDocumentById';
-
-// Queries:
-export function useGetDocuments(): UseQueryResult<DocumentDetails[]> {
-  return useQuery<DocumentDetails[]>({
-    queryKey: [getDocumentsQueryKey],
-    queryFn: () => getDocuments(),
-  } satisfies UseQueryOptions<DocumentDetails[]>);
-}
-
-export function useGetFullDocumentById(documentId: string): UseQueryResult<FullDocument | null> {
-  return useQuery<FullDocument | null>({
-    queryKey: [getDocumentByIdQueryKey, documentId],
-    queryFn: () => getFullDocumentById(documentId),
-    enabled: !!documentId,
-  } satisfies UseQueryOptions<FullDocument | null>);
-}
+import { createEvent, updateEvent, removeEvent } from '@/lib/actions/event';
+import { onSuccessCallback, onErrorCallback } from '@/lib/utils/types';
+import { getDocumentByIdQueryKey } from '@/lib/actions/document';
 
 // Mutations:
-export function useCreateDocument(onSuccessCb?: onSuccessCallback, onErrorCb?: onErrorCallback) {
+export function useCreateEvent(onSuccessCb?: onSuccessCallback, onErrorCb?: onErrorCallback) {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: async (input: Prisma.DocumentCreateInput) => createDocument(input),
+    mutationFn: async ({
+      documentId,
+      input,
+    }: {
+      documentId: string;
+      input: Prisma.EventCreateInput;
+    }) => createEvent(documentId, input),
     onSuccess: (data) => {
-      queryClient.invalidateQueries({ queryKey: [getDocumentsQueryKey] });
+      queryClient.invalidateQueries({ queryKey: [getDocumentByIdQueryKey, data.documentId] });
       if (onSuccessCb) {
         onSuccessCb(data);
       }
@@ -55,49 +31,17 @@ export function useCreateDocument(onSuccessCb?: onSuccessCallback, onErrorCb?: o
   });
 }
 
-export function useUpdateDocument(onSuccessCb?: onSuccessCallback, onErrorCb?: onErrorCallback) {
+export function useUpdateEvent(onSuccessCb?: onSuccessCallback, onErrorCb?: onErrorCallback) {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: async (input: Prisma.DocumentUpdateInput) => updateDocument(input),
-    onMutate: async (updatedDocument) => {
-      // Cancel any outgoing refetches
-      await queryClient.cancelQueries({ queryKey: [getDocumentByIdQueryKey, updatedDocument.id] });
-
-      // Snapshot the previous value
-      const previousDocumentById = queryClient.getQueryData([
-        getDocumentByIdQueryKey,
-        updatedDocument.id,
-      ]);
-
-      // If there's existing review data, merge it with the updates
-      const mergedDocument = previousDocumentById
-        ? {
-            ...previousDocumentById,
-            ...updatedDocument,
-            // If there are nested objects you need to merge them individually
-            // For example, if 'user' is a nested object and you only want to update 'user.name', you'd do:
-            // user: { ...previousReviewById.user, ...updatedReview.user },
-          }
-        : updatedDocument; // Fallback to updatedReview directly if no previous data
-
-      // Optimistically update to the new value
-      queryClient.setQueryData([getDocumentByIdQueryKey, updatedDocument.id], mergedDocument);
-
-      return { previousDocumentById };
-    },
-    onError: (error, updatedDocument, context) => {
-      // Rollback on error
-      queryClient.setQueryData(
-        [getDocumentByIdQueryKey, updatedDocument.id],
-        context?.previousDocumentById
-      );
+    mutationFn: async (input: Prisma.EventUpdateInput) => updateEvent(input),
+    onError: (error) => {
       if (onErrorCb) {
         onErrorCb(error);
       }
     },
     onSuccess: (data) => {
-      queryClient.invalidateQueries({ queryKey: [getDocumentsQueryKey] });
       queryClient.invalidateQueries({ queryKey: [getDocumentByIdQueryKey, data.id] });
       if (onSuccessCb) {
         onSuccessCb(data);
@@ -106,13 +50,12 @@ export function useUpdateDocument(onSuccessCb?: onSuccessCallback, onErrorCb?: o
   });
 }
 
-export function useRemoveDocument(onSuccessCb?: onSuccessCallback, onErrorCb?: onErrorCallback) {
+export function useRemoveEvent(onSuccessCb?: onSuccessCallback, onErrorCb?: onErrorCallback) {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: (documentId: string) => removeDocument(documentId),
+    mutationFn: (eventId: string) => removeEvent(eventId),
     onSuccess: (data) => {
-      queryClient.invalidateQueries({ queryKey: [getDocumentsQueryKey] });
       queryClient.invalidateQueries({ queryKey: [getDocumentByIdQueryKey, data.id] });
       if (onSuccessCb) {
         onSuccessCb();
