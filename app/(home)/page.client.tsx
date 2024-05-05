460010b9bd552cfa7a22e05834002d49ddf20d30
Alexander Rivera <alexander.rivera19@upr.edu>
2024-05-05 18:51:24 -0400

    Added Query var (client and bank type dropdown) to Home Page for loading before entering classification

--- app/(home)/page.client.tsx
+++ app/(home)/page.client.tsx
@@ -5,8 +5,13 @@ import { Space } from '@mantine/core';
 import HeroText from '@/components/examples/HeroText/HeroText';
 import { FeaturesCards } from '@/components/examples/FeaturesCards/FeaturesCards';
 import { PageContainer } from '@/components/global/PageContainer/PageContainer';
+import { useGetBankTypes } from '@/lib/actions/banktype';
+import { useGetClients } from '@/lib/actions/client';
 
 export function MainPage(): ReactElement {
+  const getBankTypesQuery = useGetBankTypes();
+  const getClientQuery = useGetClients();
+
   return (
     <PageContainer
       shadow="none"
